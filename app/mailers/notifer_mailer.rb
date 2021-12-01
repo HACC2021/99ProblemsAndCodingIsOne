4199fd100c2bacedf13cf634cd24c3a00f187912
Ubuntu <ubuntu@ip-172-31-29-209.us-east-2.compute.internal>
2021-12-01 00:23:07 +0000

    email notifications

--- app/mailers/notifer_mailer.rb
+++ app/mailers/notifer_mailer.rb
@@ -1,2 +1,16 @@
 class NotiferMailer < ApplicationMailer
+  
+  def pet_notifications (pets)
+    mail(to: pets.email , 
+    from: "services@gmail.com" , 
+    subject: "Pet Notification" , 
+    body: "You've been signed up for notifications following your pet registration!")
+  end
+  
+  def pet_ready (pets)
+    mail(to: pets.email ,
+    from: "services@gmail.com" ,
+    subject: "Pet Is Ready" ,
+    body: "Good news! Your pet is finally ready for pickup!")
+  end
 end
